ec42ca3395caac5e05b0160d727bace1ab88f434
swargo <sophie.junkmail@gmail.com>
2017-02-28 09:41:27 -0700

    .

--- index.js
+++ index.js
@@ -12,15 +12,12 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public'))); 
 
-// app.get('/', function(req, res){
-//     res.render('index');
-// });
 // app.get('/:viewname', function(req, res){
 //     res.render(req.params.viewname, pureJson);
 //     res.status(404).send("Sorry, can't find that page!");
 // });
 
-var urlencodedParser = bodyParser.urlencoded({ extended: true })
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 app.get('/', route.index);
 app.get('/create', route.create);
