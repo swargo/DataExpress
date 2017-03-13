86df3cbe1f3956b328a37380c9dff98ad049b8c3
swargo <sophie.junkmail@gmail.com>
2017-03-13 13:43:38 -0600

    login works, need to make authorized/isAdmin change to true/false

--- public/scripts/surveyResults.js
+++ public/scripts/surveyResults.js
@@ -5,7 +5,7 @@ var ctx = c.getContext("2d");
 var placement = 12;
 
 ctx.font="18px Josefin Sans";
-var cats = 0;
+var cats = document.getElementById("Cats").innerHTML;
 var dogs = 7;
 
 var male = 6;
